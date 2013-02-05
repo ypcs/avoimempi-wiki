9edf29af4947ee26985ba5954cc0104e8b62326c
Ville Korhonen <ville@xd.fi>
2013-02-05 23:31:14 +0200

    Added LocalSettings.confidential.php template

---
+++ conf/mediawiki/LocalSettings.confidential.template.php
@@ -0,0 +1,20 @@
+<?php
+## Database settings
+$wgDBtype           = "";
+$wgDBserver         = "";
+$wgDBname           = "";
+$wgDBuser           = "";
+$wgDBpassword       = "";
+
+## Google Analytics, Google Maps, Yahoo Maps
+$wgGoogleAnalyticsAccount = "";
+$egGoogleMapsKey = "";
+$egYahooMapsKey = "";
+
+## ReCaptcha private & public key
+$recaptcha_private_key = '';	
+$recaptcha_public_key = '';
+
+## Facebook App Id & Secret Key
+$wgFbAppId = '';
+$wgFbSecret = '';
